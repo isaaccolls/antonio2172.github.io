1f2c330b6cb0bf14fcea33d5bd0697c3ab00fce5
Isaac Colls <colls_isaac@hotmail.com>
2019-07-10 10:45:23 -0400

    navbar improvements

--- src/components/CustomNavBar.jsx
+++ src/components/CustomNavBar.jsx
@@ -4,29 +4,48 @@ import { Link } from 'react-router-dom';
 import './CustomNavBar.css';
 
 export default class CustomNavBar extends Component {
+    
+    handleSelect(eventKey) {
+        // console.log(`selected: ${eventKey}`);
+    }
+
     render() {
         return (
-            <Navbar default collapseOnSelect>
-                <Navbar.Brand>
-                    <Link to="/">CoderLife</Link>
+            <Navbar default collapseOnSelect bg="dark" variant="dark" expand="lg" sticky="top" onSelect={k => this.handleSelect(k)}>
+                <Navbar.Brand className="ml-5">
+                    I C
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse>
-                    <Nav>
-                        <Nav.Link eventKey={1} href="/" to="/">
-                            Home
-                        </Nav.Link>
-                        <Nav.Link eventKey={2} href="/about" to="/about">
-                            About
-                        </Nav.Link>
-                        <Nav.Link eventKey={3} href="/news" to="/news">
-                            News
-                        </Nav.Link>
+                    <Nav className="mx-auto" defaultActiveKey={1}>
+                        <Nav.Item className="ml-3">
+                            <Nav.Link eventKey={1} as={Link} to="/">
+                                Home
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="ml-3">
+                            <Nav.Link eventKey={2} as={Link} to="/about">
+                                About
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="ml-3">
+                            <Nav.Link eventKey={3} as={Link} to="/news">
+                                Portfolio
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="ml-3">
+                            <Nav.Link eventKey={4} as={Link} to="/about">
+                                Blog
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="ml-3">
+                            <Nav.Link eventKey={5} as={Link} to="/news">
+                                Contact
+                            </Nav.Link>
+                        </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
         );
     }
 }
-
-// <Nav.Link eventKey={3} componentClass={Link} href="/news" to="/news"> solve it
