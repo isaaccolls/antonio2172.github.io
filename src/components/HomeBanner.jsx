cd8298be5f5c0237c532ad5c643a2d67d1f2a2ac
Isaac Colls <colls_isaac@hotmail.com>
2019-07-25 17:01:38 -0400

    HomeBanner with particles

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
+import ParticleComponent from './ParticleComponent';
 import './HomeBanner.css';
 
-export default class HomeBanner extends Component {
+class HomeBanner extends Component {
 
     render() {
         return (
-            <Container id="homeBanner" className="h-100" fluid>
-                <Row className="h-100 test">
-                    <Col xs={12} className="my-auto text-center"><h1>Hello, I'm <strong>Isaac Colls</strong>.</h1><h2>Electronic and Computer Engineering</h2></Col>
-                </Row>
-            </Container>
+            <div className="h-100">
+                <ParticleComponent />
+                <Container className="h-100 homeBannerContainer" fluid>
+                    <Row className="h-100">
+                        <Col xs={3}></Col>
+                        <Col xs={6} className="my-auto text-center">
+                            <div className="homeBannerTittle">
+                                <h1>Hello, I'm <strong>Isaac Colls</strong></h1>
+                                <h2>Electronic and Computer Engineering</h2>
+                            </div>
+                        </Col>
+                        <Col xs={3}></Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
+
+export default HomeBanner;
