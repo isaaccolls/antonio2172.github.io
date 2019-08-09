358898631c77ef530057bd738d9d9ed2cf305cdc
Isaac Colls <colls_isaac@hotmail.com>
2019-08-09 13:39:48 -0400

    defining some abilities

--- src/assets/fontawesome/index.jsx
+++ src/assets/fontawesome/index.jsx
@@ -1,6 +1,17 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faCoffee, faHandPeace, faLightbulb, faUsers, faSitemap, faHandshake, faBookReader } from '@fortawesome/free-solid-svg-icons'
 import { faSmile } from '@fortawesome/free-regular-svg-icons'
 import { faLinux } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faCheckSquare, faCoffee, faSmile, faLinux);
+library.add(
+    faCheckSquare,
+    faCoffee,
+    faSmile,
+    faLinux,
+    faHandPeace,
+    faLightbulb,
+    faUsers,
+    faSitemap,
+    faHandshake,
+    faBookReader,
+);
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Image, Spinner, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Image, ProgressBar, Spinner, Alert } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import me from '../assets/images/OFON6117.JPG'
 import './Home.css';
 
 
 export default class Home extends Component {
-
 
     constructor(props) {
         super(props);
@@ -18,80 +17,76 @@ export default class Home extends Component {
     }
 
     componentDidMount() {
-        console.log("componentDidMount Home!");
         this.fetchSkills();
     }
 
     fetchSkills = () => {
-        console.log("fetchSkills started 👽");
         this.setState({ isLoading: true });
         fetch('data/skills.json')
             .then(response => {
                 if (response.ok) {
-                    console.log("respondio ok!");
                     return response.json();
                 } else {
                     throw new Error('Something went wrong...');
                 }
             })
-            .then(skills => {
-                // console.log(skills);
-                this.setState({ skills, isLoading: false });
-            })
+            .then(skills => this.setState({ skills, isLoading: false }))
             .catch(error => this.setState({ error, isLoading: false }));
     };
 
     render() {
-
-        console.log("render👽");
-        console.log(this.state.isLoading);
-        console.log("error: ", this.state.error);
-        const { skills, isLoading, error } = this.state;
-        console.log(skills);
+        let { skills, isLoading, error } = this.state;
+        skills = skills.sort((a, b) => {
+            if (a.order > b.order) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
 
         return (
             <Container id="homeContainer" className="h-100" fluid>
                 <Row className="text-center">
                     <Col xs={2}>
-                        <FontAwesomeIcon icon={["fas","check-square"]} />
-                        <h1>item 1</h1>
-                        <p>We start with a vision in our heart, and we put it on canvas.</p>
+                        <FontAwesomeIcon icon={["fas","hand-peace"]} />
+                        <h1>Resilience</h1>
+                        <p className="text-justify">Always happy with positive attitude <a href="https://en.wikipedia.org/wiki/Hakuna_matata" target="_blank">#hakunaMatata</a>.</p>
                     </Col>
                     <Col xs={2}>
-                        <FontAwesomeIcon icon={["fab", "linux"]} />
-                        <h1>item 2</h1>
-                        <p>We start with a vision in our heart, and we put it on canvas.</p>
+                        <FontAwesomeIcon icon={["fas", "lightbulb"]} />
+                        <h1>Proactive</h1>
+                        <p className="text-justify">Transforming ideas in actions and taking its responsibilities.</p>
                     </Col>
                     <Col xs={2}>
-                        <FontAwesomeIcon icon={["far","smile"]} />
-                        <h1>item 3</h1>
-                        <p>We start with a vision in our heart, and we put it on canvas.</p>
+                        <FontAwesomeIcon icon={["fas", "users"]} />
+                        <h1>Team work</h1>
+                        <p className="text-justify">Each and every individual’s personal opinion matters.</p>
                     </Col>
                     <Col xs={2}>
-                        <FontAwesomeIcon icon={["fas", "coffee"]} />
-                        <h1>item 4</h1>
-                        <p>We start with a vision in our heart, and we put it on canvas.</p>
+                        <FontAwesomeIcon icon={["fas", "sitemap"]} />
+                        <h1>Leadership</h1>
+                        <p className="text-justify">Just listening and understanding to take a common goal.</p>
                     </Col>
                     <Col xs={2}>
-                        <FontAwesomeIcon icon={["fas", "coffee"]} />
-                        <h1>item 5</h1>
-                        <p>We start with a vision in our heart, and we put it on canvas.</p>
+                        <FontAwesomeIcon icon={["fas", "handshake"]} />
+                        <h1>Committed</h1>
+                        <p className="text-justify">Understanding consequences and impacts on the final product.</p>
                     </Col>
                     <Col xs={2}>
-                        <FontAwesomeIcon icon={["fas", "coffee"]} />
-                        <h1>item 6</h1>
-                        <p>We start with a vision in our heart, and we put it on canvas.</p>
+                        <FontAwesomeIcon icon={["fas", "book-reader"]} />
+                        <h1>Self learner</h1>
+                        <p className="text-justify">Taking the initiative to formulating learning goals in an ever changing world..</p>
                     </Col>
                 </Row>
-                <Row className="text-center">
-                    <Col xs={6} className="">
+                <Row className="">
+                    <Col xs={6} className="text-center">
                         <Image src={me} rounded className="profile-pic" />
                         <h3>Who's this guy?</h3>
-                        <p>Design and implementation specialist of applications that involve hardware and software to solve problems on technological field.</p>
+                        <p className="text-justify">Design and implementation specialist of applications that involve hardware and software to solve problems on technological field.</p>
                     </Col>
-                    <Col xs={6} className="">
+                    <Col xs={6} className="text-center">
                         <h3>Skills</h3>
-                        <p>Intro about your skills goes here. Keep the list lean and only show your primary skillset. You can always provide a link to your Linkedin or Coderwall profile so people can get more info there.</p>
+                        <p className="text-justify">Since the beginning of my career, I have tried to be agnostic about technology. Where the most important to keep in mind is learning and implementing the best tool to give the best solution.</p>
                         {
                             error ?
                                 <Alert variant="danger">{`Error: ${error}`}</Alert> :
@@ -101,9 +96,14 @@ export default class Home extends Component {
                             isLoading ? 
                             <Spinner animation="grow" variant="dark" /> :
                             skills.map(skill =>
-                                    <p key={`${skill.skill.replace(/\s/g, '').toLowerCase()}`}>
-                                        {`${skill.skill} - ${skill.percent}`}
-                                    </p>
+                                    <Row key={`${skill.skill.replace(/\s/g, '').toLowerCase()}`} className="text-justify">
+                                        <Col xs={5}>
+                                            <p>{skill.skill}</p>
+                                        </Col>
+                                        <Col xs={7}>
+                                            <ProgressBar variant="dark" now={skill.percent} label={`${skill.percent}%`} />
+                                        </Col>
+                                    </Row>
                                 )
                         }
                     </Col>
