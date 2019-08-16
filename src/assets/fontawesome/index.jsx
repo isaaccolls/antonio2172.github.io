162bbe9187265418057103e3f36e9b6b8cab4b2a
Isaac Colls <colls_isaac@hotmail.com>
2019-08-16 16:44:04 -0400

    footer

--- src/assets/fontawesome/index.jsx
+++ src/assets/fontawesome/index.jsx
@@ -1,17 +1,19 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckSquare, faCoffee, faHandPeace, faLightbulb, faUsers, faSitemap, faHandshake, faBookReader } from '@fortawesome/free-solid-svg-icons'
 import { faSmile } from '@fortawesome/free-regular-svg-icons'
-import { faLinux } from '@fortawesome/free-brands-svg-icons';
+import { faHandPeace, faLightbulb, faUsers, faSitemap, faHandshake, faBookReader, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram, faFacebook, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 library.add(
-    faCheckSquare,
-    faCoffee,
     faSmile,
-    faLinux,
     faHandPeace,
     faLightbulb,
     faUsers,
     faSitemap,
     faHandshake,
     faBookReader,
+    faEnvelope,
+    faInstagram,
+    faFacebook,
+    faLinkedin,
+    faGithub,
 );
