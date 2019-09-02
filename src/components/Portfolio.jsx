86dfee5d5819367aef9b911809f2e70408563b3e
Isaac Colls <colls_isaac@hotmail.com>
2019-09-02 17:13:41 -0400

    portfolio structure

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,29 +1,99 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, ToggleButtonGroup, ToggleButton, Spinner, Alert } from 'react-bootstrap';
+import PortfolioGallery from './PortfolioGallery';
 import './Portfolio.css';
 
-export default class News extends Component {
+class Portfolio extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            portfolio: [],
+            isLoading: false,
+            error: null,
+            selectedCategory: "all",
+        }
+    }
+
+    componentDidMount() {
+        this.fetchPortfolio();
+    }
+
+    fetchPortfolio = () => {
+        this.setState({ isLoading: true });
+        fetch('data/portfolio.json')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong...');
+                }
+            })
+            .then(portfolio => {
+                this.setState({ portfolio, isLoading: false })
+            })
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
+
+    handleChange = (selectedCategory) => {
+        this.setState({ selectedCategory });
+    };
+
     render() {
+        let { portfolio, isLoading, error, selectedCategory } = this.state;
+        let categories = [...new Set(portfolio.map(portfolioItem => portfolioItem.category))];
+        categories.unshift("all");
+
         return (
-            <Container>
+            <Container id="portfolio-container" className="h-100 min-h-100 py-5" fluid={true}>
                 <Row>
-                    <h2>Projects</h2>
+                    <Col xs={12}>
+                        <h2>Projects</h2>
+                    </Col>
                 </Row>
                 <Row>
-                    <Col xs={12} sm={8} className="main-section">
+                    <Col xs={12}>
                         <p>We'll lay all these little funky little things in there. Fluff it up a little and hypnotize it.</p>
                         <p>Be so very light. Be a gentle whisper.</p>
-                        <p>The least little bit can do so much. Just let these leaves jump off the brush</p>
-                        <p>If there's two big trees invariably sooner or later there's gonna be a little tree. In your world you can create anything you desire.</p>
-                        <p>I guess I'm a little weird. I like to talk to trees and animals. That's okay though; I have more fun than most people. Without washing the brush, I'm gonna go right into some Van Dyke Brown, some Burnt Umber, and a little bit of Sap Green.</p>
-                    </Col>
-                    <Col xs={12} sm={4} className="sidebar-section">
-                        <Image src="assets/KVEE0251.JPG" />
-                        <p>Pretend you're water. Just floating without any effort. Having a good day. We're trying to teach you a technique here and how to use it. Let's put some happy little clouds in our world.</p>
-                        <p>Put your feelings into it, your heart, it's your world. If we're gonna walk though the woods, we need a little path. Let your heart take you to wherever you want to be.</p>
                     </Col>
                 </Row>
+                <Row>
+                    <Col xs={12} className="text-center">
+                        {
+                            error &&
+                            <Alert variant="danger">{`Error: ${error}`}</Alert>
+                        }
+                        {
+                            isLoading &&
+                                <Spinner animation="grow" variant="dark" />
+                        }
+                        {
+                            categories.length > 0 &&
+                            <ToggleButtonGroup
+                                name="selectCategory"
+                                type="radio"
+                                value={selectedCategory}
+                                onChange={this.handleChange}
+                                className="mb-4"
+                            >
+                                {
+                                    categories.map((categoriesItem) =>
+                                        <ToggleButton key={categoriesItem} value={categoriesItem}>{categoriesItem}</ToggleButton>
+                                    )
+                                }
+                            </ToggleButtonGroup>
+                        }
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} className="text-center">
+                        <PortfolioGallery selectedCategory={selectedCategory} portfolio={portfolio} />
+                    </Col>
+                </Row>
+
             </Container>
         );
     }
 }
+
+export default Portfolio;
