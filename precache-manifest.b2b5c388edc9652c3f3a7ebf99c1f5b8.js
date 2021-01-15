1ed938cd764a5731b1575b4519ae784001526e9a
Isaac Colls <colls_isaac@hotmail.com>
2021-01-15 14:35:28 -0300

    Updates

---
+++ precache-manifest.b2b5c388edc9652c3f3a7ebf99c1f5b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86d5c61dd0e867435e35cccd8279a2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4f9edbbf8a5777078c8",
+    "url": "./static/css/2.49695c23.chunk.css"
+  },
+  {
+    "revision": "53f0fdacc6290c8dc93a",
+    "url": "./static/css/main.b05405dc.chunk.css"
+  },
+  {
+    "revision": "b4f9edbbf8a5777078c8",
+    "url": "./static/js/2.7c723969.chunk.js"
+  },
+  {
+    "revision": "53f0fdacc6290c8dc93a",
+    "url": "./static/js/main.1c0b61ad.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d13db1fed3945c3b8c3293bfcfadb32f",
+    "url": "./static/media/Helvetica-Bold.d13db1fe.ttf"
+  },
+  {
+    "revision": "1243b706dbf774edb405221533014853",
+    "url": "./static/media/Helvetica-BoldOblique.1243b706.ttf"
+  },
+  {
+    "revision": "5e9d01a525a6945bd22c9a6c4406f75b",
+    "url": "./static/media/Helvetica-Oblique.5e9d01a5.ttf"
+  },
+  {
+    "revision": "1b580d980532792578c54897ca387e2c",
+    "url": "./static/media/Helvetica.1b580d98.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/antonio2172.github.io/precache-manifest.ea4a6b2700d7ef739372eb6026963c8d.js"
+  "./precache-manifest.b2b5c388edc9652c3f3a7ebf99c1f5b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/antonio2172.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
