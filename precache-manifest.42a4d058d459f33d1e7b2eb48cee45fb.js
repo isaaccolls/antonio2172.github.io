d769c29955ed32564a1ce333450da8f3ea38a89f
Isaac Colls <colls_isaac@hotmail.com>
2021-01-14 21:48:09 -0300

    Updates

---
+++ precache-manifest.42a4d058d459f33d1e7b2eb48cee45fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce71d5d72ea9eaabf9289c45c65ce41a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e98a14c4c367612a357",
+    "url": "./static/css/2.49695c23.chunk.css"
+  },
+  {
+    "revision": "3e8f5168b1270da162b7",
+    "url": "./static/css/main.b05405dc.chunk.css"
+  },
+  {
+    "revision": "1e98a14c4c367612a357",
+    "url": "./static/js/2.8c2f1670.chunk.js"
+  },
+  {
+    "revision": "3e8f5168b1270da162b7",
+    "url": "./static/js/main.995cea8d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d13db1fed3945c3b8c3293bfcfadb32f",
+    "url": "./static/media/Helvetica-Bold.d13db1fe.ttf"
+  },
+  {
+    "revision": "1243b706dbf774edb405221533014853",
+    "url": "./static/media/Helvetica-BoldOblique.1243b706.ttf"
+  },
+  {
+    "revision": "5e9d01a525a6945bd22c9a6c4406f75b",
+    "url": "./static/media/Helvetica-Oblique.5e9d01a5.ttf"
+  },
+  {
+    "revision": "1b580d980532792578c54897ca387e2c",
+    "url": "./static/media/Helvetica.1b580d98.ttf"
+  }
+]);
