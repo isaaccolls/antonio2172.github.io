4744b3a2ef94759d21f75b4af19e0974383ea75b
Isaac Colls <colls_isaac@hotmail.com>
2021-01-14 22:29:44 -0300

    fix router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import HomeBanner from '../HomeBanner';
 import Navbar from '../CustomNavBar';
 import Home from '../Home';
@@ -10,7 +10,7 @@ import NotFound from '../NotFound';
 import '../../assets/fonts/FontAwesome';
 
 const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <HomeBanner />
     <Navbar />
     <Switch>
