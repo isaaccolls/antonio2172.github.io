303faa04c7048b5e6ba2257a1cdf2fed4d100de5
Isaac Colls <colls_isaac@hotmail.com>
2021-03-16 14:33:00 -0300

    get data from different environments

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -7,100 +7,82 @@ import {
   SET_PROJECT_CATEGORY,
 } from './actionsTypes';
 
-const setSkillsData = payload => ({ type: SET_SKILLS_DATA, payload });
+const setSkillsData = (payload) => ({ type: SET_SKILLS_DATA, payload });
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://isaaccolls.website/' : '';
+const basePath = 'data/';
 
-export const fetchSkills = () => {
-  return dispatch => {
-    const url =
-      'https://isaaccolls.website/data/skills.json';
-    dispatch(setSkillsData({}));
-    return fetch(url)
-      .then(res => res.json())
-      .then(resJson => {
-        const { skills } = resJson;
-        const sortedSkills = skills.sort((a, b) =>
-          a.order > b.order ? 1 : -1,
-        );
-        dispatch(setSkillsData({ skills: sortedSkills }));
-      });
-  };
+export const fetchSkills = () => (dispatch) => {
+  const url = `${baseUrl}${basePath}skills.json`;
+  dispatch(setSkillsData({}));
+  return fetch(url)
+    .then((res) => res.json())
+    .then((resJson) => {
+      const { skills } = resJson;
+      const sortedSkills = skills.sort((a, b) => (a.order > b.order ? 1 : -1));
+      dispatch(setSkillsData({ skills: sortedSkills }));
+    });
 };
 
-const setExperienceData = payload => ({ type: SET_EXPERIENCE_DATA, payload });
+const setExperienceData = (payload) => ({ type: SET_EXPERIENCE_DATA, payload });
 
-export const fetchExperience = () => {
-  return dispatch => {
-    const url =
-      'https://isaaccolls.website/data/experience.json';
-    dispatch(setExperienceData({}));
-    return fetch(url)
-      .then(res => res.json())
-      .then(resJson => {
-        const { experience } = resJson;
-        const sortedExperience = experience.sort((a, b) => new Date(a.startDate) < new Date(b.startDate) ? 1 : -1);
-        dispatch(setExperienceData({ experience: sortedExperience }));
-      });
-  };
+export const fetchExperience = () => (dispatch) => {
+  const url = `${baseUrl}${basePath}experience.json`;
+  dispatch(setExperienceData({}));
+  return fetch(url)
+    .then((res) => res.json())
+    .then((resJson) => {
+      const { experience } = resJson;
+      const sortedExperience = experience.sort(
+        (a, b) => (new Date(a.startDate) < new Date(b.startDate) ? 1 : -1),
+      );
+      dispatch(setExperienceData({ experience: sortedExperience }));
+    });
 };
 
-const setEducationData = payload => ({ type: SET_EDUCATION_DATA, payload });
+const setEducationData = (payload) => ({ type: SET_EDUCATION_DATA, payload });
 
-export const fetchEducation = () => {
-  return dispatch => {
-    const url =
-      'https://isaaccolls.website/data/education.json';
-    dispatch(setEducationData({}));
-    return fetch(url)
-      .then(res => res.json())
-      .then(resJson => {
-        const { education } = resJson;
-        const sortedEducation = education.sort((a, b) =>
-          a.date > b.date ? -1 : 1,
-        );
-        dispatch(setEducationData({ education: sortedEducation }));
-      });
-  };
+export const fetchEducation = () => (dispatch) => {
+  const url = `${baseUrl}${basePath}education.json`;
+  dispatch(setEducationData({}));
+  return fetch(url)
+    .then((res) => res.json())
+    .then((resJson) => {
+      const { education } = resJson;
+      const sortedEducation = education.sort((a, b) => (a.date > b.date ? -1 : 1));
+      dispatch(setEducationData({ education: sortedEducation }));
+    });
 };
 
-const setCertificatesData = payload => ({
+const setCertificatesData = (payload) => ({
   type: SET_CERTIFICATES_DATA,
   payload,
 });
 
-export const fetchCertificates = () => {
-  return dispatch => {
-    const url =
-      'https://isaaccolls.website/data/certificates.json';
-    dispatch(setCertificatesData({}));
-    return fetch(url)
-      .then(res => res.json())
-      .then(resJson => {
-        const { certificates } = resJson;
-        const sortedCertificates = certificates.sort((a, b) =>
-          a.date > b.date ? -1 : 1,
-        );
-        dispatch(setCertificatesData({ certificates: sortedCertificates }));
-      });
-  };
+export const fetchCertificates = () => (dispatch) => {
+  const url = `${baseUrl}${basePath}certificates.json`;
+  dispatch(setCertificatesData({}));
+  return fetch(url)
+    .then((res) => res.json())
+    .then((resJson) => {
+      const { certificates } = resJson;
+      const sortedCertificates = certificates.sort((a, b) => (a.date > b.date ? -1 : 1));
+      dispatch(setCertificatesData({ certificates: sortedCertificates }));
+    });
 };
 
-const setProjectsData = payload => ({
+const setProjectsData = (payload) => ({
   type: SET_PROJECTS_DATA,
   payload,
 });
 
-export const fetchProjects = () => {
-  return dispatch => {
-    const url =
-      // 'https://us-central1-portfolio-api-77f4e.cloudfunctions.net/api/projects';
-      'https://isaaccolls.website/data/projects.json';
-    dispatch(setProjectsData({}));
-    return fetch(url)
-      .then(res => res.json())
-      .then(resJson => {
-        const { projects } = resJson; dispatch(setProjectsData({ projects }))
-      });
-  };
+export const fetchProjects = () => (dispatch) => {
+  const url = `${baseUrl}${basePath}projects.json`;
+  dispatch(setProjectsData({}));
+  return fetch(url)
+    .then((res) => res.json())
+    .then((resJson) => {
+      const { projects } = resJson; dispatch(setProjectsData({ projects }));
+    });
 };
 
-export const setProjectCategory = payload => ({ type: SET_PROJECT_CATEGORY, payload });
+export const setProjectCategory = (payload) => ({ type: SET_PROJECT_CATEGORY, payload });
