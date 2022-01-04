ec34297ae24aa840ca7914184dd9d6fa4f8bbb7f
Isaac Colls <colls_isaac@hotmail.com>
2022-01-04 20:47:10 -0300

    feat: testing prettier

--- src/components/HomeBanner/HomeBanner.jsx
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -6,23 +6,25 @@ import headerBorder from '../../assets/images/header-border.svg';
 const HomeBanner = () => (
   <div className={`d-flex align-items-start flex-column ${styles.wrapper}`}>
     <CustomParticles />
-    <div className={`my-auto mx-auto text-center py-2 px-2 py-sm-3 px-sm-5 ${styles.title}`}>
+    <div
+      className={`my-auto mx-auto text-center py-2 px-2 py-sm-3 px-sm-5 ${styles.title}`}
+    >
       <h1>
-        Hey, I&apos;m
-        {' '}
-        <br className={`.d-none .d-sm-block ${styles.hide}`} />
-        <span className="display-4">Isaac Colls</span>
+        Hey, I&apos;m <br className={`.d-none .d-sm-block ${styles.hide}`} />
+        <span className="display-4">Isaac Colls!</span>
       </h1>
       <p className={`my-1 ${styles.hide}`}>
-        Electronic and Computer Engineering
-        {' '}
-        <span role="img" aria-label="nerd face">🤓</span>
+        Electronic and Computer Engineering{' '}
+        <span role="img" aria-label="nerd face">
+          🤓
+        </span>
       </p>
       <p className={`my-1 ${styles.hide}`}>&</p>
       <p className={`my-1 ${styles.hide}`}>
-        Full Stack developer
-        {' '}
-        <span role="img" aria-label="ninja">🥷</span>
+        Full Stack developer{' '}
+        <span role="img" aria-label="ninja">
+          🥷
+        </span>
       </p>
     </div>
     <div className="w-100 mt-auto">
